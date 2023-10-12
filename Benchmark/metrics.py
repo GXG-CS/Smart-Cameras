e0ced35715bd1f44ce7727f59f053fb0cfcb6006
GXG <xiaoguang_guo@meins.edu>
2023-10-12 17:09:22 -0600

    Benchmark structure

---
+++ Benchmark/metrics.py
@@ -0,0 +1,28 @@
+# metrics.py
+
+# Shared metrics
+def mean_absolute_error(true_values, predictions):
+    ...
+    return mae
+
+# Classification metrics
+def accuracy(true_values, predictions):
+    ...
+    return acc
+
+def precision(true_values, predictions):
+    ...
+    return prec
+
+# ... [Other classification metrics] ...
+
+# Regression metrics
+def mean_squared_error(true_values, predictions):
+    ...
+    return mse
+
+def r_squared(true_values, predictions):
+    ...
+    return r2
+
+# ... [Other regression metrics] ...
---
+++ Benchmark/note.md
@@ -0,0 +1,30 @@
+folder structure:
+
+Benchmark/
+│
+├── classification/
+│   ├── classification.py
+│   └── methods/
+│
+├── regression/
+│   ├── regression.py
+│   └── methods/
+│
+├── data/
+│   └── pi3b_tf_pose_estimation_results.csv
+│
+├── preprocessing.py
+│
+└── metrics.py
+
+
+
+
+
+
+
+
+
+
+
+
