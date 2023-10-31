41ce1163502925d91343f50615da78e6676193de
GXG <xiaoguang_guo@mines.edu>
2023-10-31 12:08:34 -0600

    draw pi3b 3d

---
+++ Benchmark/draw/draw_cluster_3D.py
@@ -0,0 +1,64 @@
+import argparse
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+
+def plot_data(data_path, output_path):
+    # Check if the directory exists, if not create it
+    output_directory = os.path.dirname(output_path)
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
+
+    # Read data into a DataFrame
+    try:
+        df = pd.read_csv(data_path)
+    except FileNotFoundError:
+        print("The file was not found at the specified path.")
+        return
+
+    # Select features for visualization
+    features = ['sdram_freq', 'cpu_cores', 'cpu_freq']
+    selected_data = df[features]
+
+    # Get unique labels for coloring
+    labels = df['label'].unique()
+
+    # Create 3D plot
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    # Plot each point and color it based on its label
+    for label in labels:
+        label_data = selected_data[df['label'] == label]
+        ax.scatter(label_data['sdram_freq'], label_data['cpu_cores'], label_data['cpu_freq'], label=f'Label {label}')
+
+    # Add axis labels
+    ax.set_xlabel('SDRAM Frequency')
+    ax.set_ylabel('CPU Cores')
+    ax.set_zlabel('CPU Frequency')
+
+    # Add legend
+    ax.legend()
+
+    # Save plot to output path
+    plt.savefig(output_path)
+
+    # Show the plot
+    # plt.show()
+
+if __name__ == '__main__':
+    # Initialize argument parser
+    parser = argparse.ArgumentParser(description='Visualize selected features with different colors for each label.')
+
+    # Add arguments for data path and output path
+    parser.add_argument('--data_path', type=str, required=True, help='Path to the CSV data file.')
+    parser.add_argument('--output_path', type=str, required=True, help='Path to save the output plot.')
+
+    # Parse arguments
+    args = parser.parse_args()
+
+    # Call function to plot data
+    plot_data(args.data_path, args.output_path)
+
+
+# python draw_cluster_3D.py --data_path ../results/pi3b/tf_pose_estimation/cluster/gaussian_mixture/boxcox_4.csv --output_path ./output_plot.png
---
+++ Benchmark/draw/run_draw_cluster_3D.py
@@ -0,0 +1,31 @@
+import os
+import subprocess
+
+# Base directory where the CSV files are located
+base_directory = "../results/pi3b/tf_pose_estimation/cluster"
+
+# Relative path to the directory where you want to save all output plots
+output_directory = "pi3b/cluster/"
+
+# Create the output directory if it doesn't exist
+if not os.path.exists(output_directory):
+    os.makedirs(output_directory)
+
+# Find all CSV files in the base directory and its subdirectories
+csv_files = [os.path.join(dp, f) for dp, dn, filenames in os.walk(base_directory) for f in filenames if f.endswith('.csv')]
+
+# Loop through each CSV file and run draw_cluster_3D.py
+for csv_file in csv_files:
+    # Extract the filename to use it for saving the plot
+    filename = os.path.basename(csv_file)
+    filename_without_extension = os.path.splitext(filename)[0]
+
+    # Construct the output path for each plot using the clustering method and filename
+    clustering_method = os.path.basename(os.path.dirname(csv_file))
+    output_path = os.path.join(output_directory, f"{clustering_method}_{filename_without_extension}_plot.png")
+
+    # Run the draw_cluster_3D.py script with the current CSV file and output path
+    subprocess.run(["python", "draw_cluster_3D.py", "--data_path", csv_file, "--output_path", output_path])
+
+print(f"All plots have been saved in {output_directory}")
+
